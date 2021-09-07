6668b6a56b1daa64baa7a882a99b1b562f5c31b5
James Wilkinson <james.wilkinson@couchbase.com>
2021-09-07 15:11:21 +0100

    Added providerConfigure function to connect to Couchbase Cloud API

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+
+	"github.com/JamesWilkinsonCB/terraform-provider-couchbasecloud/provider"
 )
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: func() *schema.Provider {
-			return couchbasecloud.Provider()
+			return provider.Provider()
 		},
 	})
 }
